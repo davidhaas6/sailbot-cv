52a407a62b71f23991f1ef17af48b10daf139625
David Haas <davidhaas6@gmail.com>
2018-04-05 16:05:29 -0400

    Added video capture, made FOV into a variable

--- camera.py
+++ camera.py
@@ -4,21 +4,35 @@ import os
 import numpy as np
 import math
 
-
-
 class Camera:
-	def __init__(self):
+	def __init__(self, video_channel=1, calibration_path="./calibration/calibration_vals.pkl"):
 		# Load the undistortion parameters
-		with open('./calibration/calibration_vals.pkl', 'rb') as f:
+		with open(calibration_path, 'rb') as f:
 			DIMS, camera_matrix, dist = pickle.load(f)
 			self.map1, self.map2 = cv2.fisheye.initUndistortRectifyMap(camera_matrix, dist, np.eye(3), camera_matrix, DIMS, cv2.CV_16SC2)
 
-		self.dimensions = DIMS # width and height
-		self.f_len_x = camera_matrix[0][0] # The X focal length
+		self.DIMENSIONS = DIMS # width and height
+		self.FOCAL_LEN_X = camera_matrix[0][0] # The X focal length
+
+		fov_rad = 2 * math.atan((self.dimensions[0]/(2 * self.f_len_x)))
+		self.FOV = fov_rad * (180/math.pi)
+
+		self.video_capture = cv2.VideoCapture(video_channel)
+
+	def get_frame(self, mirrored=False, undistorted=False):
+		if self.video_capture.isOpened():
+			retval, frame = self.video_capture.read()
+			if retval:
+				if mirrored:
+					frame = cv2.flip(frame, 1)
+				if undistorted:
+					frame = self.undistort(frame)
+				return frame
+		return False
 
 	def undistort(self, img):
 		return cv2.remap(img, self.map1, self.map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
 	def FOV(self):
-		fov_rad = 2 * math.atan((self.dimensions[0]/(2 * self.f_len_x)))
+		fov_rad = 2 * math.atan((self.DIMENSIONS[0]/(2 * self.FOCAL_LEN_X)))
 		return fov_rad * (180/math.pi) # Convert to degrees
