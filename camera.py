a2546722b82fdc6d782d5ca6f48ca743e3f64d99
David Haas <davidhaas6@gmail.com>
2018-04-07 09:57:35 -0400

    Added more fidelity with the dimensions:

--- camera.py
+++ camera.py
@@ -4,31 +4,35 @@ import os
 import numpy as np
 
 class Camera:
-	def __init__(self, disable_video=False, video_channel=1, calibration_path="./calibration/calibration_vals.pkl"):
-		# Load the undistortion parameters
-		with open(calibration_path, 'rb') as f:
-			DIMS, camera_matrix, dist = pickle.load(f)
-			self.map1, self.map2 = cv2.fisheye.initUndistortRectifyMap(camera_matrix, dist, np.eye(3), camera_matrix, DIMS, cv2.CV_16SC2)
+    def __init__(self, disable_video=False, video_channel=1, calibration_path="./calibration/calibration_vals.pkl"):
+        # Load the undistortion parameters
+        with open(calibration_path, 'rb') as f:
+            DIMS, camera_matrix, dist = pickle.load(f)
+            self.map1, self.map2 = cv2.fisheye.initUndistortRectifyMap(camera_matrix, dist, np.eye(3), camera_matrix, DIMS, cv2.CV_16SC2)
 
-		self.DIMENSIONS = DIMS # width and height
-		self.FOCAL_LEN_X = camera_matrix[0][0] # The X focal length
 
-		fov_rad = 2 * np.arctan((self.DIMENSIONS[0]/(2 * self.FOCAL_LEN_X)))
-		self.FOV = np.degrees(fov_rad)
+        self.FOCAL_LEN_X = camera_matrix[0][0] # The X focal length
 
-		if not disable_video:
-			self.video_capture = cv2.VideoCapture(video_channel)
+        if not disable_video:
+            self.video_capture = cv2.VideoCapture(video_channel)
+            _, frame = self.video_capture.read()
+            self.DIMENSIONS = frame.shape
+        else:
+            self.DIMENSIONS = DIMS
 
-	def get_frame(self, mirrored=False, undistorted=False):
-		if self.video_capture.isOpened():
-			retval, frame = self.video_capture.read()
-			if retval:
-				if mirrored:
-					frame = cv2.flip(frame, 1)
-				if undistorted:
-					frame = self.undistort(frame)
-				return frame
-		return False
+        fov_rad = 2 * np.arctan((self.DIMENSIONS[0]/(2 * self.FOCAL_LEN_X)))
+        self.FOV = np.degrees(fov_rad)
 
-	def undistort(self, img):
-		return cv2.remap(img, self.map1, self.map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    def get_frame(self, mirrored=False, undistorted=False):
+        if self.video_capture.isOpened():
+            retval, frame = self.video_capture.read()
+            if retval:
+                if mirrored:
+                    frame = cv2.flip(frame, 1)
+                if undistorted:
+                    frame = self.undistort(frame)
+                return frame
+        return False
+
+    def undistort(self, img):
+        return cv2.remap(img, self.map1, self.map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
